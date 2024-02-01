e97e756d91d2513cd7785cbc9fb7d11b84ff0c2e
Shara Raj <shararaj1997@gmail.com>
2024-02-01 08:35:27 +0530

    Created components ShowList and ShowSummary

---
+++ src/components/ShowSummary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+const ShowSummary = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default ShowSummary;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as ShowList} from './ShowList'
+export {default as ShowSummary} from './ShowSummary'
+export {default as Navbar} from './Navbar'
